65a05eace1c05a57bcb6d53a128f1ed51823bc29
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-19 23:37:11 -0700

    flake.nix: refactor (again)

--- flake.nix
+++ flake.nix
@@ -3,44 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    devenv.url = "github:cachix/devenv";
-	devenv.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-	tomodachi94.url = "github:tomodachi94/dotfiles?dir=pkgs";
-	tomodachi94.inputs.nixpkgs.follows = "nixpkgs";
+	tomopkgs = {
+	  url = "github:tomodachi94/dotfiles?dir=pkgs";
+	  inputs.nixpkgs.follows = "nixpkgs";
+	};
   };
 
-  outputs = { self, nixpkgs, devenv, flake-utils, tomodachi94, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-		tomopkgs = tomodachi94.packages.${system};
-      in
-      {
-        packages.hugo-website = pkgs.stdenv.mkDerivation rec {
-          pname = "hugo-website";
-          version = "0.1.0";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.hugo tomopkgs.hugo-bearblog ];
-          buildPhase = "hugo --minify";
-          configurePhase = ''
-            mkdir ./themes
-            cp -r ${tomopkgs.hugo-bearblog} ./themes/hugo-bearblog
-          '';
-          installPhase = "mv public $out";
-        };
-        defaultPackage = self.packages.${system}.hugo-website;
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.hugo ];
-          inherit inputs pkgs;
-          modules = [
-            ({ pkgs, ... }: {
-              # This is your devenv configuration
-              packages = [ pkgs.hugo ];
-              processes.run.exec = "hugo serve";
-            })
-          ];
-        };
-      });
+  outputs = { nixpkgs, tomopkgs, ... }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+		"x86_64-darwin"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    packages = forAllSystems (pkgs: {
+	  default = pkgs.stdenvNoCC.mkDerivation rec {
+        name = "hugo-website";
+        src = ./.;
+        nativeBuildInputs = [ pkgs.hugo tomopkgs.packages.${pkgs.system}.hugo-bearblog ];
+        buildPhase = "hugo --minify";
+        configurePhase = ''
+          mkdir ./themes
+          cp -r ${tomopkgs.packages.${pkgs.system}.hugo-bearblog} ./themes/hugo-bearblog
+        '';
+        installPhase = "mv public $out";
+      };
+    });
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ pkgs.hugo ];
+	  };
+	});
+  };
 }
-
