46105533329bfc4b79ce649ed3497f07b5b6865e
Tomodachi94 <tomodachi94@protonmail.com>
2026-01-16 22:42:39 -0800

    flake.nix: reformat

--- flake.nix
+++ flake.nix
@@ -10,34 +10,41 @@
     };
   };
 
-  outputs = { nixpkgs, ai-robots-txt, ... }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-		"x86_64-darwin"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in rec {
-    packages = forAllSystems (pkgs: {
-	  default = pkgs.stdenvNoCC.mkDerivation rec {
-        name = "hugo-website";
-        src = ./.;
-        nativeBuildInputs = [ pkgs.hugo ];
-        buildPhase = "hugo --minify";
-        installPhase = ''
-	  echo "" >> "public/robots.txt"
-	  echo "# ai.robots.txt - Source: https://github.com/ai-robots-txt/ai.robots.txt" >> "public/robots.txt"
-          cat ${ai-robots-txt}/robots.txt >> "public/robots.txt"
-	  mv public $out
-	'';
-      };
-    });
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ just lychee ];
-		inputsFrom = [ packages.${pkgs.system}.default ];
-	  };
-	});
-  };
+  outputs =
+    { nixpkgs, ai-robots-txt, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    rec {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "hugo-website";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.hugo ];
+          buildPhase = "hugo --minify";
+          installPhase = ''
+            echo "" >> "public/robots.txt"
+            echo "# ai.robots.txt - Source: https://github.com/ai-robots-txt/ai.robots.txt" >> "public/robots.txt"
+            cat ${ai-robots-txt}/robots.txt >> "public/robots.txt"
+            mv public $out
+          '';
+        };
+      });
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            just
+            lychee
+          ];
+          inputsFrom = [ packages.${pkgs.system}.default ];
+        };
+      });
+    };
 }
